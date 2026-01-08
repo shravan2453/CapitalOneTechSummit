02d2e2ee749745c0169fee18e5dbbe00c494c8ba
Cara Wang <cararwang@gmail.com>
2026-01-07 18:49:37 -0700

    add Supabase connection test to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabase'; //use this in your components!
+
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <h1>Student Loan App</h1>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div>
+                <h1>Student Loan App</h1>
+            </div>
+        </main>
+    );
 }
 
